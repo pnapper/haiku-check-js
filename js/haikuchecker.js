28e314d4968f878a2b22eba87474960f703932f3
Daniel Cheng and Patrick Napper <daniel@>
2017-11-15 13:37:35 -0800

    syllable check working

--- js/haikuchecker.js
+++ js/haikuchecker.js
@@ -1,36 +1,42 @@
 export class HaikuChecker {
-  constructor(lineOne, lineTwo, lineThree) {
-    this.lineOne = lineOne;
-    this.lineTwo = lineTwo;
-    this.lineThree = lineThree;
+  constructor() {
+    // this.lineOne = lineOne;
+    // this.lineTwo = lineTwo;
+    // this.lineThree = lineThree;
   }
 
-  CheckLines() {
+  CheckLines(lineOne, lineTwo, lineThree) {
     //Checking if Haiku has three lines
     let IsHaiku = false;
 
-    if(this.lineOne !== "" && this.lineTwo !== "" && this.lineThree !== "")
+    if(lineOne !== "" && lineTwo !== "" && lineThree !== "")
     {
       IsHaiku = true;
     }
     return IsHaiku;
   }
 
-  CheckSyllables() {
+  CheckSyllables(lineOne, lineTwo, lineThree) {
     //Checking if Line 1 has 5 syllables, line 2 has 7 and line 3 has 5 syllables
 
     var syllable = require('syllable');
     let IsHaiku = false;
-    let lineOne = this.lineOne;
-    let lineTwo = this.lineTwo;
-    let lineThree = this.lineThree;
+    // let lineOne = this.mylineOne;
+    // let lineTwo = this.lineTwo;
+    // let lineThree = this.lineThree;
     let lineOneSyllable = syllable(lineOne);
     let lineTwoSyllable = syllable(lineTwo);
     let lineThreeSyllable = syllable(lineThree);
 
 
+    console.log(syllable(lineOne));
+    console.log(lineOne);
     console.log(lineOneSyllable);
+    console.log(syllable(lineTwo));
+    console.log(lineTwo);
     console.log(lineTwoSyllable);
+    console.log(syllable(lineThree));
+    console.log(lineThree);
     console.log(lineThreeSyllable);
 
     if( lineOneSyllable == 5 && lineTwoSyllable == 7 && lineThreeSyllable == 5)
--- spec/haikuchecker-spec.js
+++ spec/haikuchecker-spec.js
@@ -10,16 +10,14 @@ describe('HaikuChecker', function() {
     var haikuChecker = new HaikuChecker()
     expect(haikuChecker.CheckLines('simple1', 'line1', '1here')).toEqual(true)
   });
-  // it('should test whether all the lines in Haiku has the right syllables output false', function() {
-  //   var haikuChecker = new HaikuChecker()
-  //   expect(haikuChecker.CheckSyllables('simple1', 'line1', '1here')).toEqual(false)
-  // });
   it('should test whether all the lines in Haiku has the right syllables output false', function() {
     var haikuChecker = new HaikuChecker()
-    expect(haikuChecker.CheckSyllables('An old silent pond', 'A frog jumps into the pond', 'splash! Silence again'
-  )).toEqual(false)
+    expect(haikuChecker.CheckSyllables('simple1', 'line1', '1here')).toEqual(false)
   });
-
-
+  it('should test whether all the lines in Haiku has the right syllables output false', function() {
+    var haikuChecker = new HaikuChecker()
+    expect(haikuChecker.CheckSyllables('An old23 silent pond.', 'A frog jumps into the pond', 'splash! Silence again'
+  )).toEqual(true)
+  });
 
 });
